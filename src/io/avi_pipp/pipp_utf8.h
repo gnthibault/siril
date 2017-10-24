b6c9b14a15455f54bb4634ddf67580a34fb8350c
lock042 <cyril.richard42@gmail.com>
2017-10-24 07:41:35 +0000

    Use 64bits version of function for kFreeBSD

--- src/io/avi_pipp/pipp_utf8.h
+++ src/io/avi_pipp/pipp_utf8.h
@@ -37,8 +37,8 @@
 #define fseek64 fseeko  // DragonFly BSD, FreeBSD, OpenBSD, NetBSD
 #define ftell64 ftello  // DragonFly BSD, FreeBSD, OpenBSD, NetBSD
 #elif defined (__FreeBSD_kernel__) && defined (__GLIBC__)
-#define fseek64 fseeko  // KFreeBSD
-#define ftell64 ftello  // KFreeBSD
+#define fseek64 fseeko64  // KFreeBSD
+#define ftell64 ftello64  // KFreeBSD
 #else
 #define fseek64 _fseeki64  // Windows
 #define ftell64 _ftelli64  // Windows
