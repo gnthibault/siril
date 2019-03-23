56622c99b874491e00a84d135e63f922022075dd
Cyril Richard <cyril.richard42@gmail.com>
2019-03-23 17:36:09 +0100

    Fixes issue #341

--- src/gui/open_dialog.c
+++ src/gui/open_dialog.c
@@ -176,6 +176,7 @@ static void set_filters_dialog(GtkFileChooser *chooser, int whichdial) {
 		gtk_filter_add(chooser, _("Film Files (*.avi, *.mpg, ...)"), film_filter,
 				com.filter == TYPEAVI);
 		} else {
+			all_filter = g_string_append(all_filter, ";");
 			all_filter = g_string_append(all_filter, film_filter);
 		}
 		free(film_filter);
