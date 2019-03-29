9855da691b513c17857655ca6f7deb203132ae36
Marc-Olivier Barre <mobarre-gitlab@snarchi.io>
2019-03-29 10:31:09 +0000

    Refactoring of autotool files by @mobarre

---
+++ pixmaps/pixmap-list.mk
@@ -0,0 +1,24 @@
+pixmaps = \
+	chain.png \
+	cm.png \
+	curve.png \
+	export.png \
+	grid.png \
+	histo_dark.png \
+	histo.png \
+	list_dark.png \
+	list.png \
+	mirrorx_dark.png \
+	mirrorx.png \
+	mirrory_dark.png \
+	mirrory.png \
+	neg.png \
+	rotate-acw_dark.png \
+	rotate-acw.png \
+	rotate-cw_dark.png \
+	rotate-cw.png \
+	scale-slider-horz.png \
+	siril_logo.svg \
+	siril.svg \
+	starfinder.png \
+	sum.png
