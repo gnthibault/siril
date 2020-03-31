a0bf8b1224f7563c51a7f4b658f69d756ca079c8
Cyril Richard <cyril.richard42@gmail.com>
2020-03-31 14:45:57 +0200

    Fixing bug

--- src/algos/background_extraction.c
+++ src/algos/background_extraction.c
@@ -57,10 +57,10 @@
 #define C(i) (gsl_vector_get(c,(i)))
 
 static double poly_4(gsl_vector *c, double x, double y) {
-	double value = C(0) * C(1) * x + C(2) * y + (x * x) * C(3) + (x * y) * C(4)
+	double value = C(0) + C(1) * x + C(2) * y + (x * x) * C(3) + (x * y) * C(4)
 			+ (y * y) * C(5) + (x * x) * x * C(6) + (x * x) * y * C(7)
 			+ x * (y * y) * C(8)+ (y * y) * y * C(9) + (x * x) * (x * x) * C(10)
-			+ (x * x) * (x * y) * C(11)+ (x * x) * (y * y) * C(12)
+			+ (x * x) * (x * y) * C(11) + (x * x) * (y * y) * C(12)
 			+ (x * y) * (y * y) * C(13) + (y * y) * (y * y) * C(14);
 
 	return (value);
