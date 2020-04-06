14f2d97c924dd417f3b5789a2825d11d3dbe0ec6
Cyril Richard <cyril.richard42@gmail.com>
2020-04-06 22:19:45 +0200

    Remove unued function

--- src/algos/background_extraction.c
+++ src/algos/background_extraction.c
@@ -300,24 +300,6 @@ static unsigned int _rand(uint64_t *const p_rng) {
       return (unsigned int)*p_rng;
 }
 
-static long dither(long max) {
-	unsigned long
-	// max <= RAND_MAX < ULONG_MAX, so this is okay.
-	num_bins = (unsigned long) max + 1, num_rand = (unsigned long) RAND_MAX + 1,
-			bin_size = num_rand / num_bins, defect = num_rand % num_bins;
-
-	long x;
-
-	do {
-		x = rand();
-	}
-	// This is carefully written not to overflow
-	while (num_rand - defect <= (unsigned long) x);
-
-	// Truncated division is intentional
-	return x / bin_size;
-}
-
 static double *convert_fits_to_img(fits *fit, int channel, gboolean add_dither) {
 	struct timeval t_start, t_end;
 
