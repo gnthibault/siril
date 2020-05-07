11dfba8b52bb7769d2752b5144ef4b95decc2446
Cyril Richard <cyril.richard42@gmail.com>
2020-05-07 20:52:09 +0200

    Should fix the issue #475

--- src/io/image_format_fits.c
+++ src/io/image_format_fits.c
@@ -1221,7 +1221,10 @@ int readfits(const char *filename, fits *fit, char *realname, gboolean force_flo
 
 int siril_fits_open_diskfile(fitsfile **fptr, const char *filename, int iomode, int *status) {
 	gchar *localefilename = get_locale_filename(filename);
-	fits_open_image(fptr, localefilename, iomode, status);
+	fits_open_diskfile(fptr, localefilename, iomode, status);
+	if (!status) {
+		siril_fits_move_first_image(*fptr);
+	}
 	g_free(localefilename);
 	return *status;
 }
