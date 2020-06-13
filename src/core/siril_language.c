e3d361de2d553ea394153a31885233e3594a9248
Cyril Richard <cyril.richard42@gmail.com>
2020-06-13 14:37:02 +0200

    Add Japanese Language

--- src/core/siril_language.c
+++ src/core/siril_language.c
@@ -45,6 +45,7 @@ parsed_code locale_str[] = {
 	{"es_ES", "Espanol"},
 	{"fr", "Français"},
 	{"it_IT", "Italiano"},
+	{"ja_JP", "Japanese"},
 	{"nl_BE", "Nederlands"},
 	{"pl_PL", "Polish"},
 	{"pt_PT", "Português"},
