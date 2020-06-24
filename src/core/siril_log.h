62435c31e1f941e6a3338f386f4973809d9383f1
yomgui <yomgui1@gmail.com>
2020-06-24 17:06:28 +0000

    separate GUI implementation from core implementation.
    Fix #276

---
+++ src/core/siril_log.h
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Siril, an astronomy image processor.
+ * Copyright (C) 2005-2011 Francois Meyer (dulle at free.fr)
+ * Copyright (C) 2012-2020 team free-astro (see more in AUTHORS file)
+ * Reference site is https://free-astro.org/index.php/Siril
+ *
+ * Siril is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Siril is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siril. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SIRIL_LOG_H
+#define _SIRIL_LOG_H
+
+char* siril_log_message(const char* format, ...);
+char* siril_log_color_message(const char* format, const char* color, ...);
+
+#endif /* _SIRIL_LOG_H */
