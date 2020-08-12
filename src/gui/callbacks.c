gnthibault/siril

commit 906d26c012aa995fe94d74d6211c3c4ba6fc1a13

Display dimensions and ratio of the current selection
- while drawing it, on mouse event
- on new selection callback
- on switching page

refactor of calculate_fwhm()

src/gui/callbacks.c
@@ -667,33 +667,37 @@ int match_drawing_area_widget(GtkWidget *drawing_area, gboolean allow_rgb) {
 	return -1;
 }
 
-void calculate_fwhm(GtkWidget *widget) {
-	/* calculate and display FWHM */
-	int layer = match_drawing_area_widget(widget, FALSE);
-	if (layer != -1) {
-		gchar *buf, *label_name;
-		const char *layer_name = untranslated_vport_number_to_name(layer);
-		GtkLabel *label;
-		if (com.selection.w && com.selection.h) {// Now we don't care about the size of the sample. Minimization checks that
-			if (com.selection.w < 300 && com.selection.h < 300) {
-				double roundness;
-				double fwhm_val;
-
-				fwhm_val = psf_get_fwhm(&gfit, layer, &roundness);
-				buf = g_strdup_printf(_("fwhm = %.2f, r = %.2f"), fwhm_val,
-						roundness);
-			} else
-				buf = g_strdup_printf(_("fwhm: selection is too large"));
-		} else {
-			buf = g_strdup_printf(("fwhm: no selection"));
-		}
-		label_name = g_strdup_printf("labelfwhm%s", layer_name);
-		label = GTK_LABEL(lookup_widget(label_name));
-		gtk_label_set_text(label, buf);
-
-		g_free(label_name);
+void update_display_selection() {
+	const char *layer_name = untranslated_vport_number_to_name(com.cvport);
+	gchar *label_name = g_strdup_printf("labelselection_%s", layer_name);
+	if (com.selection.w && com.selection.h) {
+		gchar *buf = g_strdup_printf(_("w: %d h: %d ratio: %.4f"), com.selection.w, com.selection.h,
+			(double)com.selection.w / (double)com.selection.h);
+		gtk_label_set_text(GTK_LABEL(lookup_widget(label_name)), buf);
 		g_free(buf);
+	} else {
+		gtk_label_set_text(GTK_LABEL(lookup_widget(label_name)), "");
 	}
+	g_free(label_name);
+}
+
+void update_display_fwhm() {
+	gchar *buf;
+	const char *layer_name = untranslated_vport_number_to_name(com.cvport);
+	gchar *label_name = g_strdup_printf("labelfwhm%s", layer_name);
+	if (com.selection.w && com.selection.h) {// Now we don't care about the size of the sample. Minimization checks that
+		if (com.selection.w < 300 && com.selection.h < 300) {
+			double roundness;
+			double fwhm_val = psf_get_fwhm(&gfit, com.cvport, &roundness);
+			buf = g_strdup_printf(_("fwhm = %.2f, r = %.2f"), fwhm_val, roundness);
+		} else
+			buf = g_strdup_printf(_("fwhm: selection is too large"));
+	} else {
+		buf = g_strdup_printf(_("fwhm: no selection"));
+	}
+	gtk_label_set_text(GTK_LABEL(lookup_widget(label_name)), buf);
+	g_free(label_name);
+	g_free(buf);
 }
 
 /* displays the opened image file name in the layers window.
@@ -1305,6 +1309,8 @@ void initialize_all_GUI(gchar *supported_files) {
 
 	/* register some callbacks */
 	register_selection_update_callback(update_export_crop_label);
+	register_selection_update_callback(update_display_selection);
+	register_selection_update_callback(update_display_fwhm);
 
 	/* initialization of some paths */
 	initialize_path_directory();
@@ -1792,7 +1798,8 @@ void on_notebook1_switch_page(GtkNotebook *notebook, GtkWidget *page,
 	set_cutoff_sliders_values();// load the previous known values for sliders
 	set_display_mode();		// change the mode in the combo box if needed
 	redraw(com.cvport, REMAP_ONLY);
-	calculate_fwhm(com.vport[com.cvport]);
+	update_display_selection();	// update the dimensions of the selection when switching page
+	update_display_fwhm();
 }
 
 struct checkSeq_filter_data {

src/gui/callbacks.h
@@ -31,7 +31,8 @@ void set_GUI_CAMERA();
 int match_drawing_area_widget(GtkWidget *drawing_area, gboolean allow_rgb);
 const char *vport_number_to_name(int);
 const char *untranslated_vport_number_to_name(int);
-void calculate_fwhm(GtkWidget *);
+void update_display_selection();
+void update_display_fwhm();
 void display_filename();
 void set_precision_switch();
 void set_layers_for_assign();

src/gui/image_interactions.c
@@ -357,11 +357,6 @@ gboolean on_drawingarea_button_release_event(GtkWidget *widget,
 			/* we have a new rectangular selection zone,
 			 * or an unselection (empty zone) */
 			new_selection_zone();
-
-			/* calculate and display FWHM - not in event
-			 * callbacks because it's in the same file and
-			 * requires a special argument */
-			calculate_fwhm(widget);
 		} else if (mouse_status == MOUSE_ACTION_SELECT_PREVIEW1) {
 			set_preview_area(0, zoomedX, zoomedY);
 			mouse_status = MOUSE_ACTION_SELECT_REG_AREA;
@@ -392,7 +387,6 @@ gboolean on_drawingarea_button_release_event(GtkWidget *widget,
 				com.selection.h = h;
 
 				new_selection_zone();
-				calculate_fwhm(widget);
 			}
 		}
 
@@ -493,6 +487,8 @@ gboolean on_drawingarea_motion_notify_event(GtkWidget *widget,
 					com.selection.h = com.startY - zoomedY;
 			}
 		}
+		// Display the dimensions of the selection while drawing it
+		update_display_selection();
 		gtk_widget_queue_draw(widget);
 	}
 	if (inimage((GdkEvent *) event)) {

src/gui/siril3.glade
@@ -2342,7 +2342,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               </packing>
                             </child>
                             <child>
-                              <object class="GtkSeparator" id="vseparator7">
+                              <object class="GtkSeparator" id="vseparatorr1">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
                                 <property name="orientation">vertical</property>
@@ -2357,17 +2357,16 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               <object class="GtkLabel" id="labeldensity_red">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
-                                <property name="width_chars">30</property>
                               </object>
                               <packing>
                                 <property name="expand">False</property>
                                 <property name="fill">True</property>
-                                <property name="padding">4</property>
+                                <property name="padding">12</property>
                                 <property name="position">2</property>
                               </packing>
                             </child>
                             <child>
-                              <object class="GtkSeparator" id="vseparator4">
+                              <object class="GtkSeparator" id="vseparatorr2">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
                                 <property name="orientation">vertical</property>
@@ -2393,7 +2392,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               </packing>
                             </child>
                             <child>
-                              <object class="GtkSeparator" id="separator5">
+                              <object class="GtkSeparator" id="vseparatorr3">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
                                 <property name="orientation">vertical</property>
@@ -2402,6 +2401,31 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                                 <property name="expand">False</property>
                                 <property name="fill">True</property>
                                 <property name="position">5</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkLabel" id="labelselection_red">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="width_chars">30</property>
+                              </object>
+                              <packing>
+                                <property name="expand">True</property>
+                                <property name="fill">True</property>
+                                <property name="padding">3</property>
+                                <property name="position">6</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkSeparator" id="vseparatorr4">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="orientation">vertical</property>
+                              </object>
+                              <packing>
+                                <property name="expand">False</property>
+                                <property name="fill">True</property>
+                                <property name="position">7</property>
                               </packing>
                             </child>
                             <child>
@@ -2414,7 +2438,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                                 <property name="expand">True</property>
                                 <property name="fill">True</property>
                                 <property name="padding">3</property>
-                                <property name="position">6</property>
+                                <property name="position">8</property>
                               </packing>
                             </child>
                           </object>
@@ -2509,7 +2533,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               <object class="GtkLabel" id="labeldensity_green">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
-                                <property name="width_chars">30</property>
+                                <property name="width_chars">22</property>
                               </object>
                               <packing>
                                 <property name="expand">False</property>
@@ -2544,7 +2568,32 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               </packing>
                             </child>
                             <child>
-                              <object class="GtkSeparator" id="separator6">
+                              <object class="GtkSeparator" id="vseparatorg3">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="orientation">vertical</property>
+                              </object>
+                              <packing>
+                                <property name="expand">False</property>
+                                <property name="fill">False</property>
+                                <property name="position">5</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkLabel" id="labelselection_green">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="width_chars">30</property>
+                              </object>
+                              <packing>
+                                <property name="expand">True</property>
+                                <property name="fill">True</property>
+                                <property name="padding">3</property>
+                                <property name="position">6</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkSeparator" id="vseparatorg4">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
                                 <property name="orientation">vertical</property>
@@ -2552,7 +2601,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               <packing>
                                 <property name="expand">False</property>
                                 <property name="fill">True</property>
-                                <property name="position">5</property>
+                                <property name="position">7</property>
                               </packing>
                             </child>
                             <child>
@@ -2564,7 +2613,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                                 <property name="expand">True</property>
                                 <property name="fill">True</property>
                                 <property name="padding">3</property>
-                                <property name="position">6</property>
+                                <property name="position">8</property>
                               </packing>
                             </child>
                           </object>
@@ -2663,8 +2712,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               <object class="GtkLabel" id="labeldensity_blue">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
-                                <property name="tooltip_text" translatable="yes">The value stored in pointed pixels will appear here.</property>
-                                <property name="width_chars">30</property>
+                                <property name="width_chars">22</property>
                               </object>
                               <packing>
                                 <property name="expand">False</property>
@@ -2699,7 +2747,32 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               </packing>
                             </child>
                             <child>
-                              <object class="GtkSeparator" id="separator7">
+                              <object class="GtkSeparator" id="vseparatorb3">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="orientation">vertical</property>
+                              </object>
+                              <packing>
+                                <property name="expand">False</property>
+                                <property name="fill">False</property>
+                                <property name="position">5</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkLabel" id="labelselection_blue">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="width_chars">30</property>
+                              </object>
+                              <packing>
+                                <property name="expand">True</property>
+                                <property name="fill">True</property>
+                                <property name="padding">3</property>
+                                <property name="position">6</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkSeparator" id="vseparatorb4">
                                 <property name="visible">True</property>
                                 <property name="can_focus">False</property>
                                 <property name="orientation">vertical</property>
@@ -2707,7 +2780,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                               <packing>
                                 <property name="expand">False</property>
                                 <property name="fill">True</property>
-                                <property name="position">5</property>
+                                <property name="position">7</property>
                               </packing>
                             </child>
                             <child>
@@ -2719,7 +2792,7 @@ WARNING: Reducing the number of allowed processors too much will degrade perform
                                 <property name="expand">True</property>
                                 <property name="fill">True</property>
                                 <property name="padding">3</property>
-                                <property name="position">6</property>
+                                <property name="position">8</property>
                               </packing>
                             </child>
                           </object>

src/io/sequence.c
@@ -528,7 +528,7 @@ int seq_load_image(sequence *seq, int index, gboolean load_it) {
 		display_filename();		// display filename in gray window
 		set_precision_switch(); // set precision on screen
 		adjust_reginfo();		// change registration displayed/editable values
-		calculate_fwhm(com.vport[com.cvport]);
+		update_display_fwhm();
 		update_gfit_histogram_if_needed();
 		set_cursor_waiting(FALSE);
 	}

commit d5bca5df4f16b4bba312c6b98c0bae3c823973ea

Fixes small issue introduced with 906d26c0

src/gui/callbacks.c
@@ -668,6 +668,7 @@ int match_drawing_area_widget(GtkWidget *drawing_area, gboolean allow_rgb) {
 }
 
 void update_display_selection() {
+	if (com.cvport == RGB_VPORT) return;
 	const char *layer_name = untranslated_vport_number_to_name(com.cvport);
 	gchar *label_name = g_strdup_printf("labelselection_%s", layer_name);
 	if (com.selection.w && com.selection.h) {
@@ -682,6 +683,7 @@ void update_display_selection() {
 }
 
 void update_display_fwhm() {
+	if (com.cvport == RGB_VPORT) return;
 	gchar *buf;
 	const char *layer_name = untranslated_vport_number_to_name(com.cvport);
 	gchar *label_name = g_strdup_printf("labelfwhm%s", layer_name);