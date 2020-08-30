c8330a0773f3c48efac50c2b4ea0186b8c782791
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2020-08-30 23:00:29 +0200

    Fix compilation after merge: com.display_offset_x renamed to com.display_offset.x

--- src/gui/image_interactions.c
+++ src/gui/image_interactions.c
@@ -679,8 +679,8 @@ gboolean on_drawingarea_scroll_event(GtkWidget *widget, GdkEventScroll *event, g
 void on_zoom_to_fit_check_button_toggled(GtkToggleToolButton *button, gpointer data) {
 	if (gtk_toggle_tool_button_get_active(button)) {
 		com.zoom_value = -1;
-		com.display_offset_x = 0;
-		com.display_offset_y = 0;
+		com.display_offset.x = 0;
+		com.display_offset.y = 0;
 		adjust_vport_size_to_image();
 		redraw(com.cvport, REMAP_NONE);
 	} else {
@@ -693,8 +693,8 @@ void on_zoom_to_one_button_clicked(GtkToolButton *button, gpointer user_data) {
 	if (gtk_toggle_tool_button_get_active(tbutton))
 		gtk_toggle_tool_button_set_active(tbutton, FALSE);
 	com.zoom_value = 1;
-	com.display_offset_x = 0;
-	com.display_offset_y = 0;
+	com.display_offset.x = 0;
+	com.display_offset.y = 0;
 	adjust_vport_size_to_image();
 	redraw(com.cvport, REMAP_NONE);
 }
