675736434c8314bc4d5b87850a607b8bffc5620a
Cyril Richard <cyril.richard42@gmail.com>
2020-08-30 13:26:21 +0200

    Same idea than previous commit

--- src/algos/sorting.h
+++ src/algos/sorting.h
@@ -4,24 +4,25 @@
 #include "core/siril.h"	// for types
 
 /* the quicksorts */
-void quicksort_d (double *a, size_t n);
-void quicksort_f (float *a, size_t n);
-void quicksort_s (WORD *a, size_t n);
+void quicksort_d(double *a, size_t n);
+void quicksort_f(float *a, size_t n);
+void quicksort_s(WORD *a, size_t n);
 
 /* Quick median based on quick select */
-double quickmedian (WORD *a, size_t n);
-double quickmedian_float (float *a, size_t n);
+double quickmedian(WORD *a, size_t n);
+double quickmedian_float(float *a, size_t n);
 double quickmedian_double(double *a, size_t n);
-double quickmedian_int (int *a, size_t n);
+double quickmedian_int(int *a, size_t n);
 
 /* Histogram median for very large array of unsigned short */
-double histogram_median (WORD *a, size_t n, gboolean multithread);
+double histogram_median(WORD *a, size_t n, gboolean multithread);
 double histogram_median_float(float *a, size_t n, gboolean multithread);
 
 /* Sorting netnork */
-double sortnet_median (WORD *a, size_t n);
-double sortnet_median_double (double *a, size_t n);
-void sortnet (WORD *a, size_t n);
+double sortnet_median(WORD *a, size_t n);
+double sortnet_median_double(double *a, size_t n);
+double sortnet_median_float(double *a, size_t n);
+void sortnet(WORD *a, size_t n);
 
 gint strcompare(gconstpointer *a, gconstpointer *b);
 
