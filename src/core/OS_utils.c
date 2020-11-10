8b908d92f11282e837d76615cf4195731719bb83
Cyril Richard <cyril.richard42@gmail.com>
2020-11-10 16:42:41 +0100

    Remove totally bullshit and unused code for macOS

--- src/core/OS_utils.c
+++ src/core/OS_utils.c
@@ -78,16 +78,7 @@
  * @param name the path of the directory to be tested
  * @return the disk space remaining in bytes, or a negative value if error
  */
-#ifdef OS_X
-static int64_t find_space(const gchar *name) {
-	NSError *error;
-
-	NSDictionary* fileAttributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:@name
-	                                                                                   error:&error];
-	int64_t freeSpace = [[fileAttributes objectForKey:NSFileSystemFreeSize] longLongValue];
-	return freeSpace;
-}
-#elif HAVE_SYS_STATVFS_H
+#if HAVE_SYS_STATVFS_H
 static int64_t find_space(const gchar *name) {
 	struct statvfs st;
 	int64_t available;
