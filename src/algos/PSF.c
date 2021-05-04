ff89badf0b4ee5aa0c21d9db055c930f1f602f7f
Cyril Richard <cyril.richard42@gmail.com>
2021-05-04 21:19:22 +0200

    Fixing #703
    (cherry picked from commit 3395387f805b20317e402178076dd678368b52dd)

--- src/algos/PSF.c
+++ src/algos/PSF.c
@@ -735,7 +735,7 @@ void psf_display_result(fitted_PSF *result, rectangle *area) {
 	g_free(coordinates);
 }
 
-#define _2_SQRT_2_LOG2 1.55185049709
+#define _2_SQRT_2_LOG2 2.35482004503
 
 /* If the pixel pitch and the focal length are known and filled in the 
  * setting box, we convert FWHM in pixel to arcsec by multiplying
