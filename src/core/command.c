fc6f7ab9ff2c1e1f5356bd55d04db136ae38261a
Cyril Richard <cyril.richard42@gmail.com>
2021-05-13 09:12:33 +0200

    Patch by Cissou

--- src/core/command.c
+++ src/core/command.c
@@ -1389,14 +1389,16 @@ int process_bg(int nb){
 	if (!(single_image_is_loaded() || sequence_is_loaded())) return 1;
 	WORD us_bg;
 
-	double bg = background(&gfit, -1, &com.selection, TRUE);
-	if (gfit.type == DATA_USHORT) {
-		us_bg = round_to_WORD(bg);
-		bg = bg / get_normalized_value(&gfit);
-	} else if (gfit.type == DATA_FLOAT) {
-		us_bg = float_to_ushort_range(bg);
-	} else return 1;
-	siril_log_message(_("Background value: %d (%lf)\n"), us_bg, bg);
+	for (int layer = 0; layer < gfit.naxes[2]; layer++) {
+		double bg = background(&gfit, layer, &com.selection, TRUE);
+		if (gfit.type == DATA_USHORT) {
+			us_bg = round_to_WORD(bg);
+			bg = bg / get_normalized_value(&gfit);
+		} else if (gfit.type == DATA_FLOAT) {
+			us_bg = float_to_ushort_range(bg);
+		} else return 1;
+		siril_log_message(_("Background value (channel: #%d): %d (%lf)\n"), layer, us_bg, bg);
+	}
 	return 0;
 }
 
