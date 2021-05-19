830a6dc8588e6b60ccb0a3895b9a3fe916b92015
Cyril Richard <cyril.richard42@gmail.com>
2021-05-19 14:40:19 +0200

    Removing warning that appears everytime we search for sequences
    (cherry picked from commit 04de2e92a6dfc0dca37e91e0aadb7e082cedb3b1)

--- src/io/seqfile.c
+++ src/io/seqfile.c
@@ -149,9 +149,6 @@ sequence * readseqfile(const char *name){
 						if (com.pref.debayer.open_debayer)
 							seq->nb_layers = nbl_backup;
 						else {
-							siril_log_color_message(_("You are opening a SER file in CFA monochrome mode"
-									" while statistics has been saved for the three channels."
-									" Unwanted behaviors could occur.\n"), "salmon");
 							seq->nb_layers = 1;
 						}
 					}
