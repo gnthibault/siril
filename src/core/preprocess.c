262c9f02f5d8399d08dc877c4a21ee232bfc243f
cec m <cissou8@gmail.com>
2021-05-28 21:45:42 +0000

    SER debayer at preprocess

--- src/core/preprocess.c
+++ src/core/preprocess.c
@@ -292,8 +292,8 @@ static int prepro_image_hook(struct generic_seq_args *args, int out_index, int i
 	}
 
 	if (prepro->debayer) {
-		// not for SER because it is done on-the-fly
-		if (!prepro->seq || prepro->seq->type == SEQ_REGULAR || prepro->seq->type == SEQ_FITSEQ) {
+		// debayering SER is now allowed - https://gitlab.com/free-astro/siril/-/issues/549	
+		if (!prepro->seq || prepro->seq->type == SEQ_REGULAR || prepro->seq->type == SEQ_FITSEQ || prepro->seq->type == SEQ_SER ) {
 			debayer_if_needed(TYPEFITS, fit, TRUE);
 		}
 
